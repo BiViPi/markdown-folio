5906559890c5b45d8ad78513d0f0597ad3759a4b
BiViPi <phubui277@gmail.com>
2026-04-06 14:42:48 +0700

    chore: bump version to 1.1.0 and add collapsible toolbar

--- src/PreviewPanel.ts
+++ src/PreviewPanel.ts
@@ -433,6 +433,10 @@ export class PreviewPanel {
                         <svg viewBox="0 0 24 24"><path d="M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26 5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1z"/></svg>
                         <span class="tb-label">Theme</span>
                     </button>
+                    <div class="toolbar-sep"></div>
+                    <button class="toolbar-btn" id="toolbar-toggle" title="Collapse Toolbar">
+                        <svg viewBox="0 0 24 24"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </button>
                 </div>
 
                 <div id="png-dropdown" class="dropdown-menu">
